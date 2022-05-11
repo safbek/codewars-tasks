540d7066cb16689ff7a338d326b88e798466ffbd
safbek <safbek@list.ru>
2022-05-11 16:16:30 +0300

    check primeNumber

---
+++ primeNumber.js
@@ -0,0 +1,23 @@
+const isPrimeWithRecurcion = (num) => {
+    const iter = (i) => {
+        if (num < 2) return false;
+        if (num === i) return true;
+
+        if (num % i === 0) return false;
+        i++;
+        return iter(i);
+    }
+    return iter(2);
+}
+console.log(isPrimeWithRecurcion(25));
+
+const isPrimeWithLoop = (num) => {
+    if (num < 2) return false;
+ 
+    for(let i = 2; i <= num; i++) {
+        if (num === i) return true;
+        if (num % i === 0) return false;
+    }
+}
+
+console.log(isPrimeWithLoop(25));
